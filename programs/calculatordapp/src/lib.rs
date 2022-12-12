7b32e3ec6ef385e54332b6f3ac103096d02482b5
flooijdt <flooijdt@localhost.localdomain>
2022-12-12 09:41:19 -0300

    placeholder

--- programs/calculatordapp/src/lib.rs
+++ programs/calculatordapp/src/lib.rs
@@ -9,5 +9,7 @@ pub mod calculatordapp {
     use super::*;
     pub fn create(ctx: Context<Create>, init_message: String) -> ProgramResult {
         let calculator = &mut ctx.accounts.calculator;
+        calculator.greeting = init_message;
+        Ok(())
     }
 }
