c7b5f4e48842eb8e45a006728e96eefb906d95ca
flooijdt <flooijdt@localhost.localdomain>
2022-12-12 09:40:08 -0300

    placeholder

--- programs/calculatordapp/src/lib.rs
+++ programs/calculatordapp/src/lib.rs
@@ -4,5 +4,10 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
 pub mod calculatordapp {
+    use anchor_lang::solana_program::entrypoint::ProgramResult;
+
     use super::*;
+    pub fn create(ctx: Context<Create>, init_message: String) -> ProgramResult {
+        let calculator = &mut ctx.accounts.calculator;
+    }
 }
