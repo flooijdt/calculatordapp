1b194157efa35ed17a6ca88dfa31af662b04e45e
flooijdt <flooijdt@localhost.localdomain>
2022-12-12 15:42:27 -0300

    placeholder

--- tests/calculatordapp.js
+++ tests/calculatordapp.js
@@ -1,3 +1,22 @@
 const assert = require('assert')
 const anchor = require('@project-serum/anchor')
-const {SystemProgram} = anchor.web3
+const { SystemProgram } = anchor.web3
+describe('calculatordapp', () => {
+  const provider = anchor.Provider.local();
+  anchor.setProvider(provider)
+  const calculator = anchor.web3.Keypair.generate()
+  const program = anchor.workspace.Calculatordapp
+
+  it('Creates a calculator', async () => {
+    await program.rpc.create("Welcome to Solana", {
+      accounts: {
+        calculator: calculator.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId
+      },
+      signers: [calculator]
+    })
+    const account = await program.account.calculator.fetch(calculator.publicKey)
+    assert.ok(account.greeting === "Welcome to Solana")
+  })
+})
